959ed0721643714af9692a8e91190d7c23ccf660
ErikHillNTIA <ehill@ntia.gov>
2025-07-14 10:42:42 -0600

    Rename to ITM122

--- Python/Wrappers/ITM122.py
+++ Python/Wrappers/ITM122.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-"""Python wrapper for C-ITM.dll.
+"""Python wrapper for ITM122.dll.
 
-This wrapper is designed to work with ITM v1.2.2 which is written in C.
+This wrapper is designed to work with ITM v1.2.2 which is a C .dll.
 
 
 @author: Erik Hill, 2025
@@ -18,30 +18,30 @@ import numpy as np
 # local
 
 
-# %% C-ITM
-class C_ITM:
-    """Python wrapper class for ITM v1.2.2: C-ITM.dll.
+# %% ITM122
+class ITM122:
+    """Python wrapper class for ITM v1.2.2: ITM122.dll.
 
-    This class allows the use of C-ITM.dll functions from Python through a
-    ctypes interface. This class necessarily requires the C-ITM.dll to be in the
-    default repository location or a specified location. C-ITM.dll must be
+    This class allows the use of ITM122.dll functions from Python through a
+    ctypes interface. This class necessarily requires the ITM122.dll to be in the
+    default repository location or a specified location. ITM122.dll must be
     compiled for a 64-bit operating system.
 
-    C-ITM.dll function are directly linked with "dll_" prepended to their names
+    ITM122.dll function are directly linked with "dll_" prepended to their names
     and can be called directly from this wrapper. Their function signatures are
-    identical to the C-ITM.dll function signatures.
+    identical to the ITM122.dll function signatures.
 
     Additional wrapper functions for the dll_* functions are provided which
     handle the creation, passing, and parsing of the ctypes arguments. This is
     particularly convenient for input/output arguments.
 
     This wrapper class is instantiated with the file path (dll_path) to
-    C-ITM.dll. If no file path is provided, the default relative path to this
-    repository's compiled C-ITM.dll is searched.
+    ITM122.dll. If no file path is provided, the default relative path to this
+    repository's compiled ITM122.dll is searched.
 
     Usage
     -----
-    citm = C_ITM()
+    citm = ITM122()
     """
 
     # %% __init__
@@ -49,10 +49,10 @@ class C_ITM:
         self,
         dll_path: str = None
     ):
-        """Initialize C-ITM.dll wrapper.
+        """Initialize ITM122.dll wrapper.
 
-        Construct attributes for the C_ITM class by interfacing with
-        the supplied C-ITM.dll file.
+        Construct attributes for the ITM122 class by interfacing with
+        the supplied ITM122.dll file.
 
         If no dll_path is given, the default path is searched.
         If the .dll is not found on the default path, an error is given.
@@ -60,7 +60,7 @@ class C_ITM:
         Parameters
         ----------
         dll_path : str, optional (default=None)
-            The file path of the 64-bit C-ITM.dll to use.
+            The file path of the 64-bit ITM122.dll to use.
             If dll_path is not provided, the default repository location is
             searched.
         """
@@ -68,13 +68,13 @@ class C_ITM:
             os.path.join(
                 os.path.dirname(__file__),
                 r"..\..\..",
-                r"itm-longley-rice-dev\Visual_Studio\C-ITM",
+                r"itm-longley-rice-dev\Visual_Studio\ITM122",
                 r"x64\Release",
-                r"C-ITM.dll"
+                r"ITM122.dll"
             )
         )
         if dll_path is None:
-            # Try the default C-ITM.dll repository location.
+            # Try the default ITM122.dll repository location.
             dll_path = default_dll_path
 
         # Ensure the .dll exists and can be read.
@@ -94,13 +94,13 @@ class C_ITM:
         self.DLL = ct.cdll.LoadLibrary(dll_path)
 
         # Get the .dll version.
-        self.dll_version = getattr(
+        self.dll_ITMDLLVersion = getattr(
             self.DLL,
             'ITMDLLVersion'
         )
-        self.dll_version.argtypes = None
-        self.dll_version.restype = ct.c_double
-        self.dll_version = self.dll_version()
+        self.dll_ITMDLLVersion.argtypes = None
+        self.dll_ITMDLLVersion.restype = ct.c_double
+        self.dll_ITMDLLVersion = self.dll_ITMDLLVersion()
 
         # Get the point_to_point function.
         self.dll_point_to_point = getattr(
@@ -139,16 +139,16 @@ class C_ITM:
     def version(
         self,
     ):
-        """Return the C-ITM.dll version number."""
-        return self.dll_version
+        """Return the ITM122.dll version number."""
+        return self.dll_ITMDLLVersion
 
     # %% print_version
     def print_version(
         self,
     ):
-        """Print C-ITM Library version information."""
+        """Print ITM122 Library version information."""
         print((
-            "**** C-ITM Library **********************************\n"
+            "**** ITM122 Library **********************************\n"
             f"    .dll Version:      {self.version()}\n"
             f"*****************************************************"
         ))
@@ -201,7 +201,7 @@ class C_ITM:
 if __name__ == "__main__":
     print("Running program: " + os.path.basename(__file__))
 
-    citm = C_ITM()
+    citm = ITM122()
     citm.print_version()
 
     # Try point_to_point().
